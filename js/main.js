5fb75c5d766ee82b8cc9c101dc859c43818b6669
Zohid-Jurakulovich <zohidjurakulovich@gmail.com>
2022-04-02 04:43:41 +0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,116 @@
+var elList = document.querySelector(".list");
+const elBookmarkList = document.querySelector(".bookmark-list");
+
+for(var film of films){
+  
+  var newItem = document.createElement("li");
+  var newHeading = document.createElement("h3");
+  var newImg = document.createElement("img");
+  var newText = document.createElement("p");
+  
+  
+  let newBookmarkBtn = document.createElement("button");
+  // console.log(newBookmarkBtn);
+  
+  // var newTime = document.createElement("time");
+  // var newSubList = document.createElement("ul");
+  
+  
+  // for(var genre of film.genres){
+  //  var newSubItem = document.createElement("li");
+  //  newSubItem.textContent = genre;
+  
+  //  newSubList.appendChild(newSubItem);
+  
+  // }
+  
+  
+  newHeading.textContent = film.title;
+  newText.textContent = film.overview.split(" ").slice(0,20).join(" ") + " ...";
+  newBookmarkBtn.textContent = "BookMark"
+  // newTime.textContent = formatDate(film.release_date);
+  
+  
+  newItem.setAttribute("class", "list__item");
+  newImg.setAttribute("src", film.poster);
+  newImg.setAttribute("class", "list__img");
+  newBookmarkBtn.setAttribute("class", "bookmark-button")
+  newBookmarkBtn.dataset.filmId = film.id;
+  
+  
+  
+  
+  
+  newItem.appendChild(newImg);
+  newItem.appendChild(newHeading);
+  newItem.appendChild(newText);
+  newItem.appendChild(newBookmarkBtn)
+  // newItem.appendChild(newTime);
+  // newItem.appendChild(newSubList);
+  
+  
+  elList.appendChild(newItem);
+  
+}
+
+
+
+elList.addEventListener("click", evt =>{
+  const isBookmarkBtn = evt.target.matches(".bookmark-button");
+  
+  
+  
+  let bookMarks = [];
+  // console.log(bookMarks);
+  
+  if (isBookmarkBtn){
+    const filmId = evt.target.dataset.filmId;
+    
+    const foundFilm = films.find((row) => row.id === filmId);
+    
+    if (!bookMarks.includes(foundFilm)) {
+      bookMarks.push(foundFilm)
+    }
+    renderBookmarks(bookMarks, elBookmarkList)
+  }
+})
+
+elBookmarkList.addEventListener("click", evt =>{
+  if(evt.target.matches(".bookmark-remove-btn")){
+    let btnRemoveId = evt.target.dataset.dataId;
+    let findArray = bookMarks.findIndex(films => films.id == btnRemoveId);
+
+    bookMarks.splice(findArray, 1);
+
+    renderBookmarks(bookMarks, elBookmarkList);
+  }
+})
+
+
+function renderBookmarks(arr, element) {
+  
+  arr.forEach(e => {
+    
+    let bookmarkItem = document.createElement("li"); 
+    let bookmarkTitle =document.createElement("p");
+    let bookmarkRemoveBtn = document.createElement("button");
+    
+    
+    
+    
+    bookmarkTitle.textContent = e.title;
+    bookmarkRemoveBtn.textContent = "Remove";
+    
+    
+    bookmarkItem.classList.add("bookmark-item");
+    bookmarkTitle.classList.add("bookmark-title")
+    bookmarkRemoveBtn.setAttribute("class", "bookmark-remove-btn");
+    
+    bookmarkItem.appendChild(bookmarkTitle)
+    bookmarkItem.appendChild(bookmarkRemoveBtn);
+    element.appendChild(bookmarkItem);
+    // element.appendChild(bookmarkRemoveBtn);
+  });
+}
+
+// console.log(renderBookmarks);
